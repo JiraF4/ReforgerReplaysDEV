6e8b851e66783c9e9125e68b8f46ec54edf7b409
Ram <ibarahnin1@gmail.com>
2023-11-02 21:06:49 +0500

    Explosion fix

--- scripts/game/Replays/PS_ReplayExplosionWriter.c
+++ scripts/game/Replays/PS_ReplayExplosionWriter.c
@@ -7,17 +7,18 @@ class PS_ReplayExplosionWriter : ScriptComponent
 {
 	override protected void OnPostInit(IEntity owner)
 	{
-		/* There is no getters :(
-		BaseTriggerComponent trigger = BaseTriggerComponent.Cast(owner.FindComponent(BaseTriggerComponent));
-		array<BaseProjectileEffect> projectileEffects = new array<BaseProjectileEffect>();
-		trigger.GetProjectileEffects(BaseExplosionDamageEffect, projectileEffects);
-		BaseExplosionDamageEffect impulse = BaseExplosionDamageEffect.Cast(projectileEffects[0]);
-		*/
 		if (!GetGame().InPlayMode())
 			return;
 		
-		PS_ReplayWriter replayWriter = PS_ReplayWriter.GetInstance();
-		vector origin = owner.GetOrigin();
-		replayWriter.WriteExplosion(origin[0], origin[2], 10.0);
+		BaseTriggerComponent trigger = BaseTriggerComponent.Cast(owner.FindComponent(BaseTriggerComponent));
+		array<BaseProjectileEffect> projectileEffects = new array<BaseProjectileEffect>();
+		trigger.GetProjectileEffects(BaseProjectileEffect, projectileEffects);
+		
+		if (projectileEffects.Count() > 0)
+		{
+			PS_ReplayWriter replayWriter = PS_ReplayWriter.GetInstance();
+			vector origin = owner.GetOrigin();
+			replayWriter.WriteExplosion(origin[0], origin[2], 10.0);
+		}
 	}
 }
