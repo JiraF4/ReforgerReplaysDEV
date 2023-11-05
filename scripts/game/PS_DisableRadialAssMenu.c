4e58fd86f89a947c5778f1ae2cfd68f160a34df5
HubSesk <meshok.pisem29@gmail.com>
2023-11-05 21:23:30 +0500

    Disable marker system

---
+++ scripts/game/PS_DisableRadialAssMenu.c
@@ -0,0 +1,15 @@
+modded class SCR_MapRadialUI
+{
+    override protected void OnInputMenuOpen(float value, EActionTrigger reason)
+    {
+
+    }
+}
+
+modded class SCR_MapMarkersUI
+{
+    override protected void OnInputQuickMarkerMenu(float value, EActionTrigger reason)
+    {
+
+    }
+}
